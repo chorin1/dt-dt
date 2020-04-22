d7dd048c3d8833935fb4824bc65d917b5f821770
Chorinb <ben.chorin@appsflyer.com>
2020-04-22 19:49:55 +0300

    unified cancel

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v20.html"}
   :dependencies []
-  :main ^:skip-aot dtdt.core
   :profiles {:uberjar   {:aot :all}
              :dev       {:dependencies [[org.clojure/clojure "1.10.1"]]
                          :global-vars    {*warn-on-reflection* true}}})
--- src/dtdt/core.clj
+++ src/dtdt/core.clj
@@ -22,7 +22,7 @@
 
 (defn every
   "will create and run a new task that will execute every-ms milliseconds starting from initial-delay.
-  returns the created task (that can later be canceled)"
+  returns the created task (can later be canceled)"
   [every-ms f timer & {:keys [initial-delay ex-handler]
                        :or   {initial-delay 0
                               ex-handler    default-ex-hadnler}}]
@@ -31,18 +31,22 @@
     (. ^Timer timer scheduleAtFixedRate ^TimerTask task ^long initial-delay ^long every-ms)
     task))
 
-(defn at
+(defn in
   [delay-ms f timer {:keys [ex-handler]
                      :or   {ex-handler default-ex-hadnler}}]
   (let [task (create-task f ex-handler)]
     (. ^Timer timer schedule ^TimerTask task ^long delay-ms)
     task))
 
-(defn cancel-task [^TimerTask task]
-  (. task cancel))
-
-(defn last-execution-time [^TimerTask task]
+(defn last-execution-time
+  "returns last execution time of the task in epoch time"
+  [^TimerTask task]
   (. task scheduledExecutionTime))
 
-(defn cancel-timer [^Timer timer]
-  (. timer cancel))
+(defn cancel
+  "calling cancel with a timertask will cancel the task,
+  calling it with a timer will cancel all timer's tasks"
+  [t]
+  (cond
+    (instance? TimerTask t) (. ^TimerTask t cancel)
+    (instance? Timer t) (. ^Timer t cancel)))
